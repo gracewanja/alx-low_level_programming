87206cdd49e8d0ce23910790f72acf1b361d5174
gracewanja <gracewanja1999@gmail.com>
2023-03-20 14:32:29 -0700

    2-print_alphabet.c

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -9,15 +9,13 @@
 int main(void)
 {
 char letter = 'a';
-	do {
-	putchar(letter);
-	letter++;
-	}
-	while
-       	{
-		(letter <= 'z');
-	}	
-	putchar('\n'); 
-	return (0);
+do {
+putchar(letter);
+letter++;
 }
-
+while
+{
+(letter <= 'z');
+}
+putchar('\n');
+return (0); }
