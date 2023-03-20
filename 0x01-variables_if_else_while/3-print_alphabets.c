878b3eac98c5fa53471c39062e2a2d68501eed99
gracewanja <gracewanja1999@gmail.com>
2023-03-20 15:49:11 -0700

    3-print_alphabets.c

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -7,16 +7,14 @@
 int main()
 {
 	 int i;
-
-    for (i = 0; i < 26; i++) {
-        putchar('a' + i);
-    }
-
-    for (i = 0; i < 26; i++) {
-        putchar('A' + i);
-    }
-
-    putchar('\n');
-
-    return 0;
+	for (i = 0; i < 26; i++)
+	{	
+	putchar('a' + i);
+	}
+	for (i = 0; i < 26; i++) 
+	{
+	putchar('A' + i);
+	}
+	putchar('\n');
+	 return 0;
 }
