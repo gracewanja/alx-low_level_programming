98b11b71ed6a2a5675e965f0a11774c62fe0c8d6
gracewanja <gracewanja1999@gmail.com>
2023-03-20 16:54:03 -0700

    5-print_numbers.c

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/** 
+*main - Entry point
+*Description:  prints all single digit numbers of base 10 starting from 0
+*Return: Always 1 (Success)
+*/
+int main(void) 
+{
+ int i;
+ for(i = 0; i < 10; i++) 
+}
+ printf("%d\n", i);
+}
+return (0);
+}
