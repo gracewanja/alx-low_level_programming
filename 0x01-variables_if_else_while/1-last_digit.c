baf0a792d6b2453492c23ceab1b51b79bac7d4bc
gracewanja <gracewanja1999@gmail.com>
2023-03-20 12:40:11 -0700

    3-print_alphabets.c

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -14,16 +14,18 @@ int main(void)
 
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
-	if (n > 5)
+
+	printf("Last digit of %d", n, n % 10);
+	if (n % 10 > 5)
 	{
-	printf("last digit of %d is %d and is  greater than 5", n);
+	printf("and is greater than 5\n");
 	}
-	else if (n == 0)
+	else if (n % 10 == 0)
 	{
-	printf("last digit of %d is %d and is  0", n);
+	printf("and is 0\n");
 	}
 	else
 	{
-	printf("last digit of %d\n is %d\n and is  less than 6 and not 0", n);
+	printf("and is  less than 6 and not 0\n");
 	}
 	return (0); }
