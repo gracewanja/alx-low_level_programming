14acc8c958ef900325dd9e8b8758b72f9d82d6b2
gracewanja <gracewanja1999@gmail.com>
2023-03-22 07:25:48 -0700

    Test function for positive or negative

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -5,7 +5,7 @@
 * main - print if the number is positive, zero, or negative
 *
 *Description: using the main function
-*this program prins "programming is positive, zero, or negative
+*this program prints "programming is positive, zero, or negative
 *Return: 0
 */
 int main(void)
@@ -16,15 +16,15 @@ n = rand() - RAND_MAX / 2;
 /* your code goes there */
 if (n > 0)
 {
-	printf("%i is positive\n", n);
+	printf("%d is positive\n", n);
 }
 else if (n == 0)
 {
-	printf("%i is zero\n", n);
+	printf("%d is zero\n", n);
 }
 else if (n < 0)
 {
-	printf("%i is negative\n", n);
+	printf("%d is negative\n", n);
 }
 return (0);
 }
