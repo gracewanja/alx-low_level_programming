4af77ae97eb318623177b4c422f312bcbb60ce9f
gracewanja <gracewanja1999@gmail.com>
2023-03-23 14:22:12 -0700

    a function that multiplies two integers

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * print_most_numbers - print num except 2 and 4
+ * print_most_numbers - prints 01356789
  *
  * Return: void
  */
@@ -10,10 +10,10 @@ void print_most_numbers(void)
 {
 	char c;
 
-	for (c = '0'; c <= 9; c++)
+	for (c = '0'; c <= '9'; c++)
 	{
 		if (!(c == '2' || c == '4'))
-			_putchar(c);
+		_putchar(c);
 	}
 	_putchar('\n');
 }
