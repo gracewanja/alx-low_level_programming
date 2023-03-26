bb98a2c7cfd96fc1a7b3766ba9d4a777762b19f0
gracewanja <gracewanja1999@gmail.com>
2023-03-26 05:05:40 -0700

    9-fizz_buzz.c

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -1,5 +1,4 @@
 #include <stdio.h>
-#include "main.h"
 /**
  * main -Entry point
  *
@@ -11,19 +10,27 @@
  */
 int main(void)
 {
-	int i;
-
+int i;
 	for (i = 1; i <= 100; i++)
 	{
 		if (i % 15 == 0)
-			printf("FizzBuzz");
-		else if (i % 3 == 0)
-			printf("Fizz");
-		else if (i % 5 == 0)
-			printf("Buzz");
-		else
-			printf(" ");
+		{
+			printf("FizzBuzz ");
 	}
-printf('\n');
+		else if (i % 3 == 0)
+		{
+			printf("Fizz ");
+		}
+		else if (i % 5 == 0)
+		{
+			printf("Buzz ");
+		}
+
+		else
+		{
+			printf("%d ", i);
+		}
+	}
+	printf("\n");
 	return (0);
 }
