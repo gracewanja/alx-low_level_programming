36167143a202e62ea8dde261199aa0cc34991501
gracewanja <gracewanja1999@gmail.com>
2023-03-26 07:09:09 -0700

    7-print_diagonal.c

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -10,15 +10,19 @@ void print_diagonal(int n)
 	int i, j;
 
 	if (n <= 0)
-		_putchar('\n');
-	for (i = 0; i < n; i++)
 	{
-		for (j = 0; j < 1; j++)
+		_putchar('\n');
+	}
+	else
+	{
+	for (i = 1; i <= n; i++)
+	{
+		for (j = 1; j < i; j++)
 		{
 			_putchar(' ');
 		}
 		_putchar('\\');
 		_putchar('\n');
 	}
+	}
 }
-
