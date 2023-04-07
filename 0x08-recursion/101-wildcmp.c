95b3bac8fc273ad0ddae3790853fc6c3331deab6
gracewanja <gracewanja1999@gmail.com>
2023-04-06 20:01:13 -0700

    101-wildcmp.c

---
+++ 0x08-recursion/101-wildcmp.c
@@ -0,0 +1,27 @@
+/**
+ * wildcmp - compares two strings and returns 1 if they can be considered
+ * identical, otherwise returns 0
+ * @s1: the first string to compare
+ * @s2: the second string to compare, which may contain the special character *
+ *
+ * Return: 1 if the strings can be considered identical, otherwise 0
+ */
+int wildcmp(char *s1, char *s2)
+{
+	if (*s1 == '\0' && *s2 == '\0')
+		return (1);
+
+	if (*s2 == '*')
+	{
+		if (*s1 == '\0' && *(s2 + 1) == '\0')
+			return (1);
+		if (*s1 == '\0' || *(s2 + 1) == '\0')
+			return (0);
+		return (wildcmp(s1 + 1, s2) || wildcmp(s1, s2 + 1));
+	}
+
+	if (*s1 == *s2)
+		return (wildcmp(s1 + 1, s2 + 1));
+
+	return (0);
+}
