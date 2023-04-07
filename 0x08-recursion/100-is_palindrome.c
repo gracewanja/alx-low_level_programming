ce2268d6c0a478c6f7d4befda8e85773c7b20044
gracewanja <gracewanja1999@gmail.com>
2023-04-06 19:28:30 -0700

    checks for an empty string_100-is_palindrome.c

---
+++ 0x08-recursion/100-is_palindrome.c
@@ -0,0 +1,23 @@
+#include "main.h"
+#include <string.h>
+/**
+ * is_palindrome - checks if is an empty string
+ * @s: the string to check
+ * Return: 1 if s is a palindrome, 0 otherwise
+ */
+int is_palindrome(char *s)
+{
+	int len, i;
+
+	len = strlen(s);
+
+	for (i = 0; i < len / 2; i++)
+	{
+		if (s[i] != s[len - i - 1])
+		{
+			return (0);
+		}
+	}
+
+	return (1);
+}
