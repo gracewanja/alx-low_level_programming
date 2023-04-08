ab647d92d67ef4f49568411ea04cc27864f97e6b
gracewanja <gracewanja1999@gmail.com>
2023-04-08 04:49:11 -0700

    100-atoi.c

---
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+int _atoi(char *s)
+{
+	int sign = 1;
+	int result = 0;
+	int i = 0;
+
+	while (s[i] != '\0')
+	{
+		if (s[i] == '-')
+		{
+			sign *= -1;
+		}
+		else if (s[i] >= '0' && s[i] <= '9')
+		{
+			result = result * 10 + (s[i] - '0');
+		}
+		else if (result > 0)
+		{
+			break;
+		}
+		i++;
+	}
+
+	return sign * result;
+}
