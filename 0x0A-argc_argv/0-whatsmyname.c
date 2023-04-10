a86a7a461e1d1609c52d84fd98e4b3080c022c77
gracewanja <gracewanja1999@gmail.com>
2023-04-10 00:31:02 -0700

    prints program name_0-whatsmyname.c

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * main - print program name
+ * @argc: arg count
+ * @argv: arg
+ * Return: Always 0
+ */
+int main(int argc, char *argv[])
+{
+	(void) argc;
+	printf("%s\n", argv[0]);
+	return (0);
+}
