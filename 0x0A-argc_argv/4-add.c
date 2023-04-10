eaff8c9f5b97a75ef71d7990d64126786a58f643
gracewanja <gracewanja1999@gmail.com>
2023-04-10 02:17:52 -0700

    add positive numbers_4-add.c

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <ctype.h>
 /**
  * main - adds positive numbers
  * @argc: argument count
  * @argv: Array of argument
- *
  * Return: 0 if successful 1 otherwise
  */
 int main(int argc, char *argv[])
