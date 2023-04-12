68e3273712b860a3fa356d720dd418f94220209e
gracewanja <gracewanja1999@gmail.com>
2023-04-12 11:15:39 -0700

    It isn't the mountains ahead to climb that wear you out; it's the pebble in your shoe

--- 0x0B-malloc_free/100-argstostr.c
+++ 0x0B-malloc_free/100-argstostr.c
@@ -2,40 +2,38 @@
 #include <stdlib.h>
 
 /**
- * argstostr - concatenates all the arguments of the program
- * @ac: the argument count
- * @av: an array of strings containing the arguments
- * Return: a pointer to a new string, or NULL if it fails
+ * argstostr - concatenates all the arguments of a program
+ * @ac: the number of arguments
+ * @av: the arguments
+ *
+ * Return: a pointer to the new string, or NULL if it fails
  */
 char *argstostr(int ac, char **av)
 {
+	int i, m, len = 0, n = 0;
 	char *str;
-	int i, l, len, total_len = 0;
 
 	if (ac == 0 || av == NULL)
 		return (NULL);
 
 	for (i = 0; i < ac; i++)
 	{
-		len = 0;
-		while (av[i][len])
+		for (m = 0; av[i][m]; m++)
 			len++;
-		total_len += len + 1;
+		len++;
 	}
-	str = malloc(total_len * sizeof(char));
+
+	str = malloc(sizeof(char) * (len + 1));
 	if (str == NULL)
 		return (NULL);
 
-	for (i = 0, l = 0; i < ac; i++)
+	for (i = 0; i < ac; i++)
 	{
-		len = 0;
-		while (av[i][len])
-		{
-			str[l++] = av[i][len++];
-		}
-		str[l++] = '\n';
+		for (m = 0; av[i][m]; m++)
+			str[n++] = av[i][m];
+		str[n++] = '\n';
 	}
-	str[l] = '\0';
+	str[n] = '\0';
 
 	return (str);
 }
