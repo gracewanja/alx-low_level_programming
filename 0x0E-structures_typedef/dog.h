6efd797685315c341eb17a979ea09a519f995602
gracewanja <gracewanja1999@gmail.com>
2023-04-17 04:50:44 -0700

    a struct containing the basic information about a dog

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,20 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - a struct containing the basic information about a dog
+ * @name: the name of the dog
+ * @age: the age of the dog
+ * @owner: the owner of the dog
+ *
+ * Description: a struct containing the basic information about a dog,
+ *              including its name, age, and owner.
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+#endif
